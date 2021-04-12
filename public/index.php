58e82f2a3f8da358204a8e2f27a6d9f246517f63
unknown <diego.juarezh@gmail.com>
2021-04-12 01:48:16 -0500

    Corregir error de CORS

--- public/index.php
+++ public/index.php
@@ -7,7 +7,21 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
+//  ERROR DE CORS
 define('LARAVEL_START', microtime(true));
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Credentials: true');
+header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS");
+header('Access-Control-Max-Age: 1000');
+header("Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization");
+//  ERROR DE CORS
+// header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+// header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+/*$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}*/
+//  ERROR DE CORS
 
 /*
 |--------------------------------------------------------------------------
