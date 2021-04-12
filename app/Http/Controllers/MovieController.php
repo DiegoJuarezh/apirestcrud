89465bd9141952a1e0a9a6eb649a53f3f32c213c
unknown <diego.juarezh@gmail.com>
2021-04-12 01:11:37 -0500

    añadimos una ruta/recurso en api.php para llamar todos los metodos del controlador MovieController, y en MovieController dejamos solo los metodos que necesitamos

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -14,17 +14,7 @@ class MovieController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        echo "HOLA";
     }
 
     /**
@@ -34,28 +24,6 @@ class MovieController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Movie  $movie
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Movie $movie)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Movie  $movie
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Movie $movie)
     {
         //
     }
